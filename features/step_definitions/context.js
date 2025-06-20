3e211f60d244ead814412c9103b7c6977f0202c8
Siilarsi Lorenzen <siilarsi@gmail.com>
2025-06-20 07:28:52 +0200

    Refactor BDD steps into separate files

---
+++ features/step_definitions/context.js
@@ -0,0 +1,9 @@
+module.exports = {
+  browser: null,
+  context: null,
+  page: null,
+  lastShipPos: null,
+  lastPointer: { x: 0, y: 0 },
+  initialAmmo: 0,
+  lastOrbPos: null
+};
---
+++ features/step_definitions/navigation.js
@@ -0,0 +1,35 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const path = require('path');
+const ctx = require('./context');
+
+Given('I open the game page', async () => {
+  const filePath = path.resolve(__dirname, '../../index.html');
+  await ctx.page.goto('file://' + filePath);
+});
+
+When('I click the start screen', async () => {
+  await ctx.page.click('#start-screen');
+});
+
+Then('the promo animation should be shown', async () => {
+  await ctx.page.waitForSelector('#promo-animation', { state: 'visible' });
+});
+
+Then('the game should appear after a short delay', async () => {
+  await ctx.page.waitForSelector('#game', { state: 'visible' });
+  const display = await ctx.page.$eval('#game', el => getComputedStyle(el).display);
+  if (display === 'none') {
+    throw new Error('Game did not appear');
+  }
+});
+
+When('I reload the page', async () => {
+  await ctx.page.reload({ waitUntil: 'load' });
+});
+
+Then('the game should not be visible', async () => {
+  const display = await ctx.page.$eval('#game', el => getComputedStyle(el).display);
+  if (display !== 'none') {
+    throw new Error('Game should not be visible');
+  }
+});
